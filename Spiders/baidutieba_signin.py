48ad72692909d0db88c3dac93191d217ca435094
RainbowC0 <67918391+RainbowC0@users.noreply.github.com>
2020-07-23 11:03:00 +0800

    Update baidutieba_signin.py

--- Spiders/baidutieba_signin.py
+++ Spiders/baidutieba_signin.py
@@ -1,44 +1,73 @@
-# !/usr/bin/env python
+# !/user/bin/env python
 # -*- coding:utf-8 -*- 
 # time: 2018/2/27--19:24
+__author__ = 'Henry'
 
 '''
 项目:百度贴吧一键签到
 目标网址:https://tieba.baidu.com/
-内容:实现一键将关注的贴吧全自动签到
+内容:实现一键将关注的贴吧全部签到
 '''
 
-import sys
 import requests, re
+import hashlib
+import sys
+from urllib import parse
 
+def encodeData(data):
+        '''主要是计算sign的值'''
+        SIGN_KEY='tiebaclient!!!'
+        s=''
+        keys=data.keys()
+        #print(sorted(keys))
+        for i in sorted(keys):
+            s+=str(i)+'='+str(data[i])
+        sign=hashlib.md5((s+SIGN_KEY).encode('utf-8')).hexdigest().upper()
+        #print(sign)
+        data.update({'sign':str(sign)})
+        return data
 
 def signin():
     print('*' * 30 + '百度贴吧签到小助手' + '*' * 30)
     cookie = sys.argv[1]
-    url = 'https://tieba.baidu.com/'
+    BDUSS = re.search(r"BDUSS=([0-9A-Za-z~\\-]*);",cookie)
+    #input('请输入您登录百度贴吧后获取的Cookie值:')
+    url = 'https://tieba.baidu.com/mo/q/newmoindex'
     headers = {
         'Cookie': cookie,
-        'User-Agent': 'bdtb for Android 8.0.8.0'
+        'User-Agent':'bdtb for Android 9.1.0.0'
+        #'Mozilla/5.0 (Linux; Android 5.1; OPPO R7s Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36 tieba/9.1.0.0 subType/mini'
     }
-    html = requests.get(url, headers=headers).text
-    tieba = re.findall(r'lp=1030">(.*?)</a>', html)
-    #tieba = tieba[:int(len(tieba) / 2)]
+    html = requests.get(url, headers=headers).json()
+    print(html)
     print('正在进行贴吧签到...')
     num = 0
-    for i in tieba:
-        #i = i.replace('\\\\', '\\').encode('latin-1').decode('unicode_escape')
-        url = 'https://tieba.baidu.com/sign/add'
-        form = {'ie': 'utf-8',
-                'kw': i,  # 要签到的贴吧名
-                'tbs': '7dc070643ef556f71594255318'}
-        html = requests.post(url, data=form, headers=headers).json()
-        if html['no'] == 1101:
-            print('[' + i + '吧]:' + '亲，此贴吧您之前已经签过了哦!')
-        if html['error'] == '' or html['no'] == 0:
-            print('[' + i + '吧]:' + '签到成功!')
+    for i in html["data"]["like_forum"]:
+        url = 'http://c.tieba.baidu.com/c/c/forum/sign'
+        form = {
+        "BDUSS":BDUSS,
+        "_client_id":"wappc_1595418910913_863",
+        "_client_type":"2",
+        "_client_version":"9.1.0.0",
+        "_phone_imei":"869411025969204",
+        "cuid":"9313BAE7943B4443E95A5EA22A296057|O",
+        "cuid_galaxy2":"9313BAE7943B4443E95A5EA22A296057|O",
+        "fid":i["forum_id"],
+        "kw":str(i["forum_name"]),
+        "net_type":1,
+        "tbs":str(html["data"]["itb_tbs"])}
+        d=encodeData(form)
+        d=parse.urlencode(d)
+        d=d.encode("utf-8")
+        htm = requests.post(url,data=d, headers=headers).json()
+        print(htm)
+        if htm['error_code'] == '160002':
+            print('[' + i["forum_name"] + '吧]:' + '亲，此贴吧您之前已经签过了哦!')
+        if htm['error_code'] == '0':
+            print('[' + str(i["forum_name"]) + '吧]:' + '签到成功! 经验+'+htm['user_info']['sign_bonus_point'])
             num += 1
     print('\n')
-    print('恭喜您,贴吧签到成功! 一共签到' + str(num) + '个贴吧!')
+    print('恭喜您,贴吧签到成功!一共签到' + str(num) + '个贴吧!')
 
 
 if __name__ == '__main__':
