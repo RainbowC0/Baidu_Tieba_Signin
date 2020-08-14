ea9f5216281afcab02978bdb99483abe2e930cbd
RainbowC0 <67918391+RainbowC0@users.noreply.github.com>
2020-08-14 13:01:04 +0800

    Update zrbsign.py

--- Spiders/zrbsign.py
+++ Spiders/zrbsign.py
@@ -7,10 +7,10 @@ import sys
 def signin():
 	print('*' * 30 + '众人帮每日自动打卡' + '*' * 30)
 	cookie = sys.argv[3]
-	url = 'http://m.zrb.net/api/units/DailyColock'
+	url = 'https://c.29592.net/api/units/DailyColock'
 	headers = {
 		'Cookie': cookie,
-		'User-Agent':'Mozilla/5.0 (Linux; Android 9; Unspecified Device) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.0.0 Mobile Safari/537.36',
+		'User-Agent':'Mozilla/5.0 (Linux; Android 5.1; OPPO R7s Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36 Html5Plus/1.0',
 		'Accept':'application/json'
 	}
 	form = {'account':sys.argv[1],'password':sys.argv[2]}
