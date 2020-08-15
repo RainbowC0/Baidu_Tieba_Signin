2b63838c3838d8cd6a85d118a78f1a31a29d635b
RainbowC0 <67918391+RainbowC0@users.noreply.github.com>
2020-08-15 20:38:06 +0800

    Update zrbling.py

--- Spiders/zrbling.py
+++ Spiders/zrbling.py
@@ -8,7 +8,7 @@ def signin():
 	print('*' * 30 + '众人帮每日自动打卡' + '*' * 30)
 	cookie = sys.argv[3]
 	url = 'https://c.29592.net/api/units/LingDailyBonus'
-	url2='https://m.zrb.net/api/units/LingDailyBonus'
+	url2='http://m.zrb.net/api/units/LingDailyBonus'
 	headers = {
 		'Cookie': cookie,
 		'User-Agent':'Mozilla/5.0 (Linux; Android 5.1; OPPO R7s Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36 Html5Plus/1.0',
