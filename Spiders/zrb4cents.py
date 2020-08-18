54e3f27673d7b022f1f782d416549d51069ddd2e
RainbowC0 <67918391+RainbowC0@users.noreply.github.com>
2020-08-18 16:38:12 +0800

    Update zrb4cents.py

--- Spiders/zrb4cents.py
+++ Spiders/zrb4cents.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 def signin():
 	#print('*' * 30 + '幸运冲冲冲每日参与' + '*' * 30)
 	url = 'http://upread.ccdy.cn/lxshow.html?c=8E671739&r='
-	ua='User-Agent':'Mozilla/5.0 (Linux; Android 5.1; OPPO R7s Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36 MicroMessenger/7.0.12.1620(0x27000C34) Process/appbrand0 NetType/WIFI Language/zh_CN ABI/arm64'
+	ua='Mozilla/5.0 (Linux; Android 5.1; OPPO R7s Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36 MicroMessenger/7.0.12.1620(0x27000C34) Process/appbrand0 NetType/WIFI Language/zh_CN ABI/arm64'
 	options = webdriver.FirefoxOptions()
 	options.add_argument('-headless')
 	profile = webdriver.FirefoxProfile()
