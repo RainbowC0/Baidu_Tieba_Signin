167442f3d30a9d7ee4259edd967ddd919bebe6df
RainbowC0 <67918391+RainbowC0@users.noreply.github.com>
2020-08-18 23:05:06 +0800

    Update zrb4cents.py

--- Spiders/zrb4cents.py
+++ Spiders/zrb4cents.py
@@ -8,37 +8,47 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def urlupread(dri):
-	return dri.current_url.find('upread.ccdy.cn')==-1 and hasbody(dri)
+	return dri.current_url.find('upread.ccdy.cn')==-1 and len(dri.find_elements_by_tag_name('script'))>2
 
 def urlwxqq(dri):
-	return dri.current_url.find('mp.weixin.qq.com')!=-1 and hasbody(dri)
+	return dri.current_url.find('mp.weixin.qq.com')!=-1 and dri.find_element_by_id('moon_inline')!=None
 
 def hasbody(dri):
 	return len(dri.find_elements_by_tag_name('body'))>0
 
 def signin():
 	#print('*' * 30 + '幸运冲冲冲每日参与' + '*' * 30)
-	url = 'http://upread.ccdy.cn/lxshow.html?c=8E671739&r='
-	ua='Mozilla/5.0 (Linux; Android 5.1; OPPO R7s Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36 MicroMessenger/7.0.12.1620(0x27000C34) Process/appbrand0 NetType/WIFI Language/zh_CN ABI/arm64'
-	options = webdriver.FirefoxOptions()
-	options.add_argument('-headless')
-	profile = webdriver.FirefoxProfile()
-	profile.set_preference('permissions.default.image',2)
-	profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false')
-	profile.set_preference('general.useragent.override',ua)
-	browser = webdriver.Firefox(options=options,firefox_profile = profile)
-	browser.get(url)
-	n=0
-	WebDriverWait(browser,5).until(EC.url_contains('mp.weixin.qq.com'))
-	while browser.current_url.find('cpu.baidu.com')==-1:
-		srs=browser.page_source
-		print(len(srs))
-		browser.back()
-		WebDriverWait(browser,10).until(urlupread)
-		n+=1
-	print(browser.page_source)
-	print(n)
-	browser.close()
+	headers={
+		'User-Agent':'Mozilla/5.0 (Linux; Android 5.1; OPPO R7s Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045318 Mobile Safari/537.36',
+		'Cookie':sys.argv[1]
+	}
+	rslt=requests.post('https://c.29592.net/next/ViewTaskStart',headers=headers).json()
+	if rslt['code']==200:
+		url = 'http://upread.ccdy.cn/lxshow.html?c=8E671739&r='
+		ua='Mozilla/5.0 (Linux; Android 5.1; OPPO R7s Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36 MicroMessenger/7.0.12.1620(0x27000C34) Process/appbrand0 NetType/WIFI Language/zh_CN ABI/arm64'
+		options = webdriver.FirefoxOptions()
+		options.add_argument('-headless')
+		options.add_argument('--ignore-certificate-errors')
+		profile = webdriver.FirefoxProfile()
+		profile.set_preference('permissions.default.image',2)
+		profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false')
+		profile.set_preference('general.useragent.override',ua)
+		browser = webdriver.Firefox(options=options,firefox_profile = profile)
+		browser.get(url)
+		n=0
+		WebDriverWait(browser,5).until(urlwxqq)
+		while browser.current_url.find('cpu.baidu.com')==-1:
+			print(len(browser.page_source))
+			browser.back()
+			WebDriverWait(browser,10).until(urlupread)
+			n+=1
+		WebDriverWait(browser,5).until(hasbody)
+		browser.close()
+		print(n)
+		rlsc=requests.post('https://c.29592.net/next/ViewTaskComplete',headers=headers).json()
+		print(rslc['msg'])
+	else:
+		print(rslt['msg'])
 
 if __name__ == '__main__':
 	signin()
